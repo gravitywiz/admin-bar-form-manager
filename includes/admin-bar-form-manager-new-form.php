4ef87520f04045d99c5ed3608efaf6107ce4f874
sbassah <sbassah@gmail.com>
2022-04-07 16:01:07 +0000

    Fixed Missing letter B issue in field label in GF2.6

--- includes/admin-bar-form-manager-new-form.php
+++ includes/admin-bar-form-manager-new-form.php
@@ -173,6 +173,9 @@ class GW_ABFM_New_Form {
 				var gfCreateField = window.CreateField;
 				window.CreateField = function( nextId, type, index ) {
 					var field = gfCreateField( nextId, type, index );
+					if ( type == 'submit' ) {
+						return field;
+					}
 					if( field.label === '<?php _e( 'Untitled', 'gravityforms' ); ?>' ) {
 						field.label = fieldTypeLabels[ field.type ];
 					}
